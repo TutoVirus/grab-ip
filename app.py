62120975ff6aeed9d7628556139b91d79cd693e2
TutoVirus <73782109+TutoVirus@users.noreply.github.com>
2021-03-10 15:46:42 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, send_file
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    print('Someone accessed')
+    return 'Hi'
+
+@app.route('/get_image')
+def get_image():
+    requester = request.remote_addr
+    print(requester)
+    return send_file('img.jpg')
+
+if __name__ == '__main__':
+      app.run(threaded=True, port=80)

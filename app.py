cf5a6bd484b035362015c828589f82700ca8d9da
TutoVirus <73782109+TutoVirus@users.noreply.github.com>
2021-03-10 17:24:38 +0000

    Update app.py

--- app.py
+++ app.py
@@ -7,18 +7,18 @@ def main():
     return 'Hi'
 
 @app.route('/pepelove.jpg')
-def get_image():
+def get_image_pepelove():
     requester = request.remote_addr
     print('IP: ' + request.headers['X-Forwarded-For'])
         
-    return send_file('img.jpg')
+    return send_file('pepelove.jpg')
 
 @app.route('/aligator.jpg')
-def get_image():
+def get_image_aligator():
     requester = request.remote_addr
     print('IP: ' + request.headers['X-Forwarded-For'])
         
-    return send_file('find_aligator.jpg')
+    return send_file('aligator.jpg')
 
 if __name__ == '__main__':
       app.run(threaded=True, port=80)
